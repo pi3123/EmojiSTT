633bf5b2c1cb7c79063582d4b156b4f05ecac8d6
Subhash S <unboxer04@gmail.com>
2021-07-02 15:03:35 -0700

    Documented

--- config.py
+++ config.py
@@ -1,11 +1,26 @@
-audioFolder = "X:\\Projects\\EmojiTTS\\Data\\Audio"
-dbFile = "X:\\Projects\\EmojiTTS\\Data\\table.json"
-logFile = "X:\\Projects\\EmojiTTS\\output.txt"
+class SpecAI:
+    """
+    Paths for training SpecAI
+    """
+    audioFolder = "X:\\Projects\\EmojiTTS\\Data\\Audio"
+    dbFile = "X:\\Projects\\EmojiTTS\\Data\\table.json"
+    logFile = "X:\\Projects\\EmojiTTS\\output.txt"
 
-specFolder = "X:\\Projects\\EmojiTTS\\Data\\Spec"
+    specFolder = "X:\\Projects\\EmojiTTS\\Data\\Spec"
 
-trainFolder = "X:\\Projects\\EmojiTTS\\Data\\Spec\\Train"
-trainCSV = "X:\\Projects\\EmojiTTS\\Data\\Spec\\train.csv"
+    trainFolder = "X:\\Projects\\EmojiTTS\\Data\\Spec\\Train"
+    trainCSV = "X:\\Projects\\EmojiTTS\\Data\\Spec\\train.csv"
 
-testFolder = "X:\\Projects\\EmojiTTS\\Data\\Spec\\Test"
-testCSV = "X:\\Projects\\EmojiTTS\\Data\\Spec\\test.csv"
+    testFolder = "X:\\Projects\\EmojiTTS\\Data\\Spec\\Test"
+    testCSV = "X:\\Projects\\EmojiTTS\\Data\\Spec\\test.csv"
+
+    SpecModelsFolder = "X:\\Projects\\EmojiTTS\\Models\\Spec"
+    TextModelsFolder = "X:\\Projects\\EmojiTTS\\Models\\Text"
+
+
+class TextEmoAI:
+    """
+    paths for training text AI
+    """
+    trainPath = "X:\\Projects\\EmojiTTS\\Data\\Text\\train"
+    testPath = "X:\\Projects\\EmojiTTS\\Data\\Text\\test"
--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from sklearn.feature_extraction.text import CountVectorizer
 
 # General
 import json
@@ -10,6 +13,7 @@ import os
 import csv
 import random
 from tqdm import tqdm
+import pandas as pd
 
 # Image processing
 from Specto import Turtle as visTurtle
@@ -24,10 +28,21 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 class databaseHelper:
     def __init__(self, filename):
+        """
+        :param filename: Storage filename
+        """
         self.dbFile = filename
 
     @staticmethod
     def moveFiles(testFiles, trainFiles, TargetFolder, FolderFiles):
+        """
+
+        :param testFiles: Testing files path
+        :param trainFiles: Training files path
+        :param TargetFolder: Folder to move the files to
+        :param FolderFiles: Dict with {Folder:csvfile}
+        :return:
+        """
         for i in tqdm(testFiles, desc="Moving files to Test folder"):
             try:
                 os.rename(i, i.replace(TargetFolder, list(FolderFiles.keys())[0]))
@@ -40,6 +55,12 @@ class databaseHelper:
                 pass
 
     def findQuality(self, filename, quality):
+        """
+        To look up the json file for a quality
+        :param filename: name of the file to find the quality
+        :param quality: quality to find
+        :return: quality
+        """
         with open(self.dbFile, 'r') as f:
             table = json.loads(f.read())
 
@@ -53,6 +74,10 @@ class databaseHelper:
         return table[filename][quality]
 
     def makeCSV(self, FolderFiles):
+        """
+        To make and save CSV file for training
+        :param FolderFiles: Dict with {Folder:csvfile}
+        """
         for folder in FolderFiles:
             with open(FolderFiles[folder], "w") as f:
                 fieldnames = ['filenames', 'emotion']
@@ -70,9 +95,16 @@ class databaseHelper:
                                     'emotion': emotion})
 
     def prepareData(self, FolderFiles, TargetFolder, split=0.2):
+        """
+        Driver code
+        :param FolderFiles: Dict with {Folder:csvfile}
+        :param TargetFolder: Storage folder
+        :param split: percentage of evaluating files
+        """
+
         # Moving files
         files = glob.glob(TargetFolder + "\\*.png")
-        random.shuffle(files)  # shuffling all the files so all of them can be equally trained
+        random.shuffle(files)
         print("Shuffled! PNG folder")
 
         testFiles = files[: int((split * len(files)))]
@@ -108,9 +140,14 @@ class visHelper:
         img.save(fname, "PNG")
 
 
-class aiHelper:
+class SpecAiHelper:
     @staticmethod
     def OneConvLayer(input_shape):
+        """
+        function for 1 conv layer model
+        :param input_shape: shape of the image
+        :return: model structure
+        """
         model = Sequential()
         model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
         model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -124,6 +161,11 @@ class aiHelper:
 
     @staticmethod
     def TwoConvLayer(input_shape):
+        """
+        function for 2 conv layer model
+        :param input_shape: shape of the image
+        :return: model structure
+        """
         model = Sequential()
         model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
         model.add(Conv2D(64, (3, 3), activation='relu'))
@@ -137,6 +179,11 @@ class aiHelper:
 
     @staticmethod
     def ThreeConvLayer(input_shape):
+        """
+        function for 3 conv layer model
+        :param input_shape: shape of the image
+        :return: model structure
+        """
         model = Sequential()
         model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
         model.add(MaxPooling2D((2, 2)))
@@ -158,6 +205,11 @@ class aiHelper:
 
     @staticmethod
     def FourConvLayer(input_shape):
+        """
+        function for 4 conv layer model
+        :param input_shape: shape of the image
+        :return: model structure
+        """
         model = Sequential()
         model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
         model.add(BatchNormalization())
@@ -190,6 +242,12 @@ class aiHelper:
         return model
 
     def getModel(self, modelID, input_shape):
+        """
+
+        :param modelID: ID of the model to return
+        :param input_shape: shape of the img
+        :return: model
+        """
         if modelID == 1:
             return self.OneConvLayer(input_shape)
         elif modelID == 2:
@@ -198,3 +256,51 @@ class aiHelper:
             return self.ThreeConvLayer(input_shape)
         elif modelID == 4:
             return self.FourConvLayer(input_shape)
+
+
+class TextAiHelper:
+    @staticmethod
+    def clean_data(text):
+        """
+        :param text: dirty text
+        :return: cleaned, stemmed text
+        """
+        stemmer = PorterStemmer()
+
+        try:
+            clean_text = text.str.lower()
+            clean_text = clean_text.str.replace('\d+', '')
+            clean_text = clean_text.str.strip()
+            clean_text = clean_text.str.replace('[^\w\s]', ' ')
+            clean_text = clean_text.str.replace('br', '')
+            clean_text = clean_text.str.replace(' +', ' ')
+            clean_text = clean_text.str.replace('\d+', '')
+
+            stop = stopwords.words('english')
+            stop.extend(["movie", "movies", "film", "one"])
+            clean_text = clean_text.apply(lambda x: " ".join(x for x in x.split() if x not in stop))
+            # Stemming
+            clean_text = clean_text.apply(lambda x: " ".join(stemmer.stem(x) for x in x.split()))
+
+            return clean_text
+        except Exception as e:
+            print("In Exception of clean_data: ", e)
+            return None
+
+    @staticmethod
+    def tokenization(df_reviews):
+        """
+        :param df_reviews: reviews
+        :return: tokenized text
+        """
+        count_vec = CountVectorizer(analyzer='word', tokenizer=lambda doc: doc, lowercase=False, max_df=0.70,
+                                    min_df=100)
+        print(" Tokenize the Reviews")
+
+        df_reviews["Clean_Review"] = df_reviews["Clean_Review"].astype(str).str.strip().str.split('[\W_]+')
+
+        words_vec = count_vec.fit(df_reviews["Clean_Review"])
+        bag_of_words = words_vec.transform(df_reviews["Clean_Review"])
+        tokens = count_vec.get_feature_names()
+        df_words = pd.DataFrame(data=bag_of_words.toarray(), columns=tokens)
+        return df_words
